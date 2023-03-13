9d4d6628718faef9e1a87bda2fe9e55521d9e446
Franco Perez <franco9274@hotmail.com>
2023-03-12 22:57:49 -0300

    Primer añadido

---
+++ Js/englishjs.js
@@ -0,0 +1,317 @@
+var items = [
+'Did you wake up early today?',
+'Have you finished your homework yet?',
+'Will you be attending the party tonight?',
+'Can you play the piano?',
+'Would you like to go for a walk in the park?',
+'Is it raining outside?',
+'Did you watch the game last night?',
+'Should we order pizza or make pasta for dinner?',
+'Have you ever been skydiving?',
+'Are you feeling well today?',
+'Did you see the news this morning?',
+'Can you speak Spanish fluently?',
+'Would you like to go to the beach this weekend?',
+'Is it snowing outside?',
+'Did you finish your work on time?',
+'Should we go to the concert or the movie theater?',
+'Have you ever been scuba diving?',
+'Are you allergic to any food?',
+'Did you enjoy the book you read last week?',
+'Can you solve this math problem?',
+'Would you like to go on a road trip?',
+'Is it hot outside?',
+'Did you have breakfast this morning?',
+'Should we go for a hike or a bike ride?',
+'Have you ever traveled abroad?',
+'Are you a fan of horror movies?',
+'Did you see the latest episode of your favorite TV show?',
+'Can you play chess?',
+'Would you like to visit a theme park or a water park?',
+'Is it windy outside?',
+'Did you finish your project before the deadline?',
+'Should we go to the zoo?',
+'Have you ever ridden a horse?',
+'Are you comfortable with public speaking?',
+'Did you watch the sunrise this morning?',
+'Can you cook a three-course meal?',
+'Would you like to go on a cruise?',
+'Is it thundering outside?',
+'Did you finish your house chores for the day?',
+'Should we go to the gym or do yoga?',
+'Have you ever gone bungee jumping?',
+'Are you a morning person?',
+'Did you read the newspaper today?',
+'Can you swim?',
+'Would you like to go camping or stay in a hotel?',
+'Is it humid outside?',
+'Did you finish your shopping at the mall?',
+'Should we go to the park?',
+'Have you ever gone zip-lining?',
+'Are you good at playing video games?',
+'Did you exercise this morning?',
+'Can you speak more than one language?',
+'Would you like to go to a music festival?',
+'Is it cloudy outside?',
+'Did you finish your paperwork at the office?',
+'Should we go to the beach or the pool?',
+'Have you ever gone rock climbing?',
+'Are you afraid of heights?',
+'Did you listen to music today?',
+'Can you ride a motorcycle?',
+'Have you ever ridden in a hot air balloon?',
+'Do you prefer coffee in the morning?',
+'Can you juggle?',
+'Would you like to go on a safari?',
+'Is it cold outside?',
+'Did you have a good weekend?',
+'Should we go to the mountains for vacation?',
+'Have you ever gone snowboarding?',
+'Are you a dog person?',
+'Did you remember to call your friend?',
+'Can you ride a unicycle?',
+'Would you like to go to a dance club?',
+'Is it a holiday today?',
+'Did you have lunch yet?',
+'Should we go to the city for a day trip?',
+'Have you ever gone kayaking?',
+'Are you a night shower person?',
+'Did you watch a movie last night?',
+'Can you play a musical instrument?',
+'Would you like to go to a vineyar?',
+'Is it foggy outside?',
+'Did you finish your errands for the day?',
+'Should we go to the aquarium?',
+'Have you ever gone parasailing?',
+'Are you a good dancer?',
+'Did you have a good workout today?',
+'Can you hula hoop?',
+'Would you like to go to a science museum?',
+'Is it a special occasion today?',
+'Did you eat dinner yet?',
+'Should we go to the amusement park?',
+'Have you ever gone wakeboarding?',
+'Are you a good cook?',
+'Did you take a nap today?',
+'Can you do a handstand?',
+'Would you like to go to a fashion show?',
+'Is it a full moon tonight?',
+'Did you clean your room today?',
+'Should we go to the botanical garden?',
+'Have you ever gone water skiing?',
+'Are you good at photography?',
+'Did you buy anything new recently?',
+'Can you do a backflip?',
+'Would you like to go to a music festival?',
+'Is it a clear night sky tonight?',
+'Did you spend time with your family today?',
+'Should we go to the planetarium?',
+'Have you ever gone windsurfing?',
+'Are you a good listener?',
+'Did you drink enough water today?',
+'Can you do a cartwheel?',
+'Would you like to go to a book festival?',
+'Is it a national holiday today?',
+'Did you learn something new today?',
+'Should we go to the jazz club?',
+'Have you ever gone rafting?',
+'Are you good at solving puzzles?',
+'Did you see any wildlife today?',
+'Can you do a split?',
+'Would you like to go to a flea market?',
+'Is it a full house tonight?',
+'Did you volunteer for anything recently?',
+'Should we go to the opera?',
+'Have you ever traveled to another continent?',
+'Do you believe in love at first sight?',
+'Did you have a good day at work today?',
+'Can you speak a second language fluently?',
+'Have you ever gone scuba diving?',
+'Do you enjoy spending time alone?',
+'Did you meet any new people today?',
+'Can you sing well?',
+'Have you ever ridden a motorcycle?',
+'Do you consider yourself a spiritual person?',
+'Did you have a good meal today?',
+'Can you cook a meal from scratch?',
+'Have you ever gone zip lining?',
+'Do you like to read books?',
+'Did you have any important meetings today?',
+'Can you run a mile in under 10 minutes?',
+'Have you ever gone bungee jumping?',
+'Do you believe in ghosts?',
+'Did you learn something new today?',
+'Can you dance the tango?',
+'Have you ever gone skydiving?',
+'Do you enjoy watching sports?',
+'Did you make any progress on a personal project today?',
+'Can you play chess?',
+'Have you ever gone rock climbing?',
+'Do you like to go to the beach?',
+'Did you have a good workout today?',
+'Can you do a push-up?',
+'Have you ever gone surfing?',
+'Do you believe in aliens?',
+'Can you do a pull-up?',
+'Have you ever gone camping?',
+'Do you like to go to concerts?',
+'Did you have any important phone calls today?',
+'Have you ever gone skiing?',
+'Do you enjoy hiking?',
+'Did you make any new connections today?',
+'Can you do a plank for a minute?',
+'Have you ever gone on a cruise?',
+'Do you like to watch movies?',
+'Did you have a good conversation with someone today?',
+'Can you swim well?',
+'Have you ever gone horseback riding?',
+'Do you believe in destiny?',
+'Did you accomplish any goals today?',
+'Can you do a handstand push-up?',
+'Have you ever gone on a road trip?',
+'Do you like to go to museums?',
+'Did you meet any deadlines today?',
+'Can you ride a unicycle?',
+'Have you ever gone on a hot air balloon ride?',
+'Do you like to watch TV shows?',
+'Did you have any unexpected surprises today?',
+'Can you do a cartwheel?',
+'Have you ever gone to a music festival?',
+'Do you enjoy trying new foods?',
+'Did you attend any events today?',
+'Can you do a backflip?',
+'Have you ever gone on a helicopter ride?',
+'Do you like to go to the theater?',
+'Did you learn anything useful today?',
+'Can you do a one-arm push-up?',
+'Have you ever gone on a river cruise?',
+'Do you like to watch documentaries?',
+'Did you receive any compliments today?',
+'Can you do a pistol squat?',
+'Have you ever gone parasailing?',
+'Did you learn anything interesting today?',
+'Can you juggle?',
+'Have you ever gone on a safari?',
+'Do you like to go to amusement parks?',
+'Did you have a productive day today?',
+'Can you solve a crossword puzzle?',
+'Have you ever gone to a music awards show?',
+'Do you enjoy playing video games?',
+'Did you make any breakthroughs today?',
+'Can you play a musical instrument?',
+'Have you ever gone on a gondola ride?',
+'Do you like to go to the zoo?',
+'Did you have any setbacks today?',
+'Can you do a muscle-up?',
+'Have you ever gone on a segway tour?',
+'Do you like to go to the circus?',
+'Did you achieve any milestones today?',
+'Can you paint a picture?',
+'Have you ever gone on a whale watching tour?',
+'Do you enjoy going to the fair?',
+'Did you overcome any challenges today?',
+'Can you sculpt something?',
+'Have you ever gone on a deep sea fishing trip?',
+'Do you like to go to the aquarium?',
+'Did you have any unexpected difficulties today?',
+'Can you write a poem?',
+'Have you ever gone on a wine tasting tour?',
+'Do you like to go to the opera?',
+'Did you have any positive feedback today?',
+'Can you solve a Sudoku puzzle?',
+'Have you ever gone on a dog sledding adventure?',
+'Do you like to go to the ballet?',
+'Did you have any moments of clarity today?',
+'Can you speak a rare language?',
+'Have you ever gone on a ghost tour?',
+'Do you like to go to the symphony?',
+'Did you receive any recognition today?',
+'Can you solve a riddle?',
+'Have you ever gone on a hot spring tour?',
+'Do you like to go to the planetarium?',
+'Did you have any significant realizations today?',
+'Can you create a website?',
+'Have you ever gone on a volcano tour?',
+'Do you like to go to the botanical garden?',
+'Did you have any profound thoughts today?',
+'Can you program a computer?',
+'Have you ever gone on a glacier hike?',
+'Do you like to go to the science museum?',
+'Did you have any enlightening experiences today?',
+'Can you solve a programming problem?',
+'Have you ever gone on a polar bear tour?',
+'Do you like to go to the natural history museum?',
+'Did you have any spiritual experiences today?',
+'Can you design a building?',
+'Have you ever gone on a desert tour?',
+'Do you like to go to the art museum?',
+'Did you have any introspective moments today?',
+'Can you sew something from scratch?',
+'Have you ever gone on a caving adventure?',
+'Do you like to go to the history museum?',
+'Did you have any transcendental experiences today?',
+'Can you create a 3D model?',
+'Have you ever gone on a camel safari?',
+]
+
+var item = items[Math.floor(Math.random()*items.length)];
+document.getElementById("question").innerHTML = item;
+
+var score = 0;
+var message = "Responde estas preguntas"
+
+function checkAnswers() {
+    const answer = document.getElementById("text").value.toLowerCase();
+    const currentQuestion = document.getElementById("question").innerHTML;
+    if (currentQuestion.includes('Do') && (answer === "yes, i do" || answer === "no, i don't")) {
+       score++;
+       var item = items[Math.floor(Math.random()*items.length)];
+       document.getElementById("question").innerHTML = item;
+       document.getElementById("score").innerHTML = score;
+       document.getElementById("message").innerHTML = message;
+    }else if (currentQuestion.includes('Have') && (answer === "yes, i have" || answer === "no, i haven't")) {
+        score++;
+        var item = items[Math.floor(Math.random()*items.length)];
+        document.getElementById("question").innerHTML = item;
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("message").innerHTML = message;
+    } else if (currentQuestion.includes('Did') && (answer === "yes, i did" || answer === "no, i didn't")) {
+        score++;
+        var item = items[Math.floor(Math.random()*items.length)];
+        document.getElementById("question").innerHTML = item;
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("message").innerHTML = message;
+    } else if (currentQuestion.includes('Should') && (answer === "yes, we should" || answer === "no, we shouldn't")) {
+        score++;
+        var item = items[Math.floor(Math.random()*items.length)];
+        document.getElementById("question").innerHTML = item;
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("message").innerHTML = message;
+    } else if (currentQuestion.includes('Can') && (answer === "yes, i can" || answer === "no, i can't")) {
+        score++;
+        var item = items[Math.floor(Math.random()*items.length)];
+        document.getElementById("question").innerHTML = item;
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("message").innerHTML = message;
+    } else if (currentQuestion.includes('Is') && (answer === "yes, it is" || answer === "no, it isn't")) {
+        score++;
+        var item = items[Math.floor(Math.random()*items.length)];
+        document.getElementById("question").innerHTML = item;
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("message").innerHTML = message;
+    } else if (currentQuestion.includes('Would') && (answer === "yes, i would" || answer === "no, wouldn't")) {
+        score++;
+        var item = items[Math.floor(Math.random()*items.length)];
+        document.getElementById("question").innerHTML = item;
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("message").innerHTML = message;
+    } else if (currentQuestion.includes('Are') && (answer === "yes, i am" || answer === "no, i am not")) {
+        score++;
+        var item = items[Math.floor(Math.random()*items.length)];
+        document.getElementById("question").innerHTML = item;
+        document.getElementById("score").innerHTML = score;
+        document.getElementById("message").innerHTML = message;
+    } else {
+        document.getElementById("message").innerHTML = "Incorrecto";
+    }
+}
